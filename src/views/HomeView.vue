f419e784f7aeeb71b3b8df8e9fbfc5241970dcb9
anyathegreat <anya.rudenko.03@bk.ru>
2024-07-11 23:29:34 +0900

    fix: clean up code

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,29 +1,7 @@
-<script setup>
-import BaseButton from '@/components/BaseButton.vue'
-import { ref } from 'vue'
-
-const isSended = ref(false)
-
-function send() {
-  isSended.value = true
-
-  setTimeout(() => {
-    isSended.value = false
-  }, 5000)
-}
-</script>
+<script setup></script>
 
 <template>
   <h1>Hello world?</h1>
-  <div class="card">
-    <BaseButton vSize="sm" vVariant="solid" vType="primary" @click="send" :vLoading="isSended">Нажми меня</BaseButton>
-  </div>
 </template>
 
-<style scoped>
-.card {
-  width: 500px;
-  height: 500px;
-  background-color: white;
-}
-</style>
+<style scoped></style>
