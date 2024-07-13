aba519977e68482e6d8df38d2bdb23130faa1bb7
anyathegreat <anya.rudenko.03@bk.ru>
2024-07-14 05:34:04 +0900

    fix: pages refactoring

--- src/components/BasePage.vue
+++ src/components/BasePage.vue
@@ -3,19 +3,11 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <aside class="v-sidebar"></aside>
   <main class="v-page"><RouterView /></main>
 </template>
 
 <style scoped>
 .v-page {
-  flex: 10;
   height: 100%;
-  background-color: black;
-}
-.v-sidebar {
-  flex: 2;
-  height: 100%;
-  background-color: palevioletred;
 }
 </style>
