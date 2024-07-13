938ba820a2bbe1eccc051fc1d5974ae6bd84d06d
anyathegreat <anya.rudenko.03@bk.ru>
2024-07-14 05:32:58 +0900

    fix: remove color

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -23,10 +23,12 @@ import ThemeChangeButton from '@/components/ThemeChangeButton.vue'
   justify-content: space-between;
   position: sticky;
   top: 0;
+  z-index: 100;
+  box-shadow: 0 3px 9px 0 var(--header-shadow-color);
   padding: 8px;
   width: 100%;
   height: 80px;
-  background-color: gray;
+  background-color: var(--header-bg-color);
 }
 
 .v-header-navigation {
