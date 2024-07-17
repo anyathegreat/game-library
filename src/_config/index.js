b19752693764f8f409eacb77d549d0df37296807
Ytar <ytar.99@yandex.ru>
2024-07-17 21:52:07 +0300

    update: add dogs API env

--- src/_config/index.js
+++ src/_config/index.js
@@ -1,4 +1,6 @@
 export const config = {
   CATS_API_URL: import.meta.env.VITE_APP_CATS_API_URL || "",
   CATS_API_KEY: import.meta.env.VITE_APP_CATS_API_KEY || "",
+  DOGS_API_URL: import.meta.env.VITE_APP_DOGS_API_URL || "",
+  DOGS_API_KEY: import.meta.env.VITE_APP_DOGS_API_KEY || "",
 };
