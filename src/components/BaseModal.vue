e60d52d0c6e7578bb7663c008cbbb9f717b20a47
anyathegreat <anya.rudenko.03@bk.ru>
2024-07-31 03:56:20 +0900

    update: add modal

---
+++ src/components/BaseModal.vue
@@ -0,0 +1,53 @@
+<script setup>
+import { computed } from "vue";
+
+const { vFullscreen } = defineProps({
+  vFullscreen: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const modalClasses = computed(() => {
+  return {
+    "v-modal-overlay": true,
+    "v-modal-overlay-fullscreen": vFullscreen,
+  };
+});
+</script>
+
+<template>
+  <div :class="modalClasses">
+    <div class="v-modal">
+      <slot name="header"></slot>
+      <slot name="content"></slot>
+      <slot name="footer"></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.v-modal-overlay {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+.v-modal-overlay-fullscreen {
+  position: fixed;
+  z-index: 4000;
+  width: 100dvw;
+  height: 100dvh;
+}
+.v-modal {
+  width: 300px;
+  overflow: hidden;
+  background-color: var(--surface);
+  border-radius: 8px;
+}
+</style>
---
+++ src/components/modals/ConfirmDialog.vue
@@ -0,0 +1,68 @@
+<script setup>
+import BaseButton from "../BaseButton.vue";
+import BaseModal from "../BaseModal.vue";
+
+const emit = defineEmits(["onCancel", "onSubmit"]);
+
+function handleCancel() {
+  emit("onCancel");
+}
+
+function handleSubmit() {
+  emit("onSubmit");
+}
+console.log(emit);
+</script>
+
+<template>
+  <BaseModal>
+    <template #header>
+      <div class="v-modal-header">
+        <button @click="handleCancel">X</button>
+      </div>
+    </template>
+    <template #content>
+      <div class="v-modal-content">Are you sure?</div>
+    </template>
+    <template #footer>
+      <div class="v-modal-footer">
+        <BaseButton vType="secondary" class="v-modal-button" @click="handleCancel">No</BaseButton>
+        <BaseButton vType="primary" class="v-modal-button" @click="handleSubmit">Yes</BaseButton>
+      </div>
+    </template>
+  </BaseModal>
+</template>
+
+<style scoped lang="scss">
+.v-modal-header {
+  display: flex;
+  justify-content: end;
+  align-items: center;
+  width: 100%;
+  height: 3rem;
+  padding: 8px 4px;
+  background-color: var(--surfaceContainer);
+}
+.v-modal-content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  padding: 8px 4px;
+  min-height: 3rem;
+  text-align: center;
+}
+.v-modal-footer {
+  display: flex;
+  justify-content: end;
+  align-items: center;
+  gap: 12px;
+  width: 100%;
+  height: 3rem;
+  padding: 8px 4px;
+  background-color: var(--surfaceContainer);
+}
+.v-modal-button {
+  width: 60px;
+}
+</style>
